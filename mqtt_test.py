01e80859755153196b44a2bb0b521ac5fe6b6e57
Rimi Reza Duity <duity@therapservices.net>
2023-05-17 14:24:33 +0600

    Added MQTT Publish client

---
+++ mqtt_test.py
@@ -0,0 +1,46 @@
+import paho.mqtt.client as mqtt
+
+import random
+import time
+
+broker = 'localhost'
+port = 1883
+topic = 'test/paho/python'
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Bad Request")
+
+    client = mqtt.Client(client_id)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def publish(client):
+    msg_count = 0
+    while True:
+        time.sleep(1)
+        msg = f"message count {msg_count}"
+        result = client.publish(topic, msg)
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+
+
+if __name__ == '__main__':
+    run()
